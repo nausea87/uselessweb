17aff8e351b5da7992551420eda96fc5c7248a8c
Erik <62840687+nausea87@users.noreply.github.com>
2020-12-07 01:00:52 +0100

    just a test for netlify

---
+++ thescript.js
@@ -0,0 +1,10 @@
+
+
+
+//Idea: Button that sends a random message?
+
+const pressedButton =
+document.getElementsByTagName("button")[0];
+   pressedButton.addEventListener("click", function (event) {
+      alert("Installing malware T-X100 v 1.04...")
+   });
